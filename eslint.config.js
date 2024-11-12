bc8eb1e2958aba78c2b3d9790817449fb8153d19
sjy <1979974477@qq.com>
2024-11-12 10:40:24 +0800

    路由设计

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue, { rules } from 'eslint-plugin-vue'
 
 export default [
   {
@@ -10,6 +10,9 @@ export default [
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  rules:{
+    'vue/multi-word-component-names':0
   },
 
   js.configs.recommended,
