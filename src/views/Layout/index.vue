4f224974485a0d799b3483c3c80754705009858f
sjy <1979974477@qq.com>
2024-11-12 16:09:00 +0800

    layout模块静态模板搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,11 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
+</script>
 <template>
-  <div>我是首页</div>
-  let foon
+  <layout-nav></layout-nav>
+  <layout-header></layout-header>
   <router-view></router-view>
+  <layout-footer></layout-footer>
 </template>
