831f2d2d161144bfe5e25ac4d3f6fd5a10109ce3
sjy <1979974477@qq.com>
2024-11-12 20:13:21 +0800

    整体结构拆分，分类结果渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,17 @@
-<template>
-  <div>我是home页</div>
-</template>
-<style scoped>
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeGoods from './components/HomeGoods.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
 
-</style>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeGoods />
+  <HomeHot />
+  <homeProduct />
+</template>
