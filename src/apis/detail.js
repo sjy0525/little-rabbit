cc398f7b10d0b90c0b9c541a1288d7f3e7ec4ac7
sjy <1979974477@qq.com>
2024-11-15 18:54:41 +0800

    热榜的渲染

---
+++ src/apis/detail.js
@@ -0,0 +1,21 @@
+import httpInstance from "@/utils/http";
+
+export const getDetail=(id)=>{
+  return httpInstance({
+    url:'/goods',
+    params:{
+      id
+    }
+  })
+}
+
+export const fetchHotGoodsAPI = ({ id, type, limit = 3 }) => {
+  return httpInstance({
+    url:'/goods/hot',
+    params:{
+      id,
+      type,
+      limit
+    }
+  })
+}
